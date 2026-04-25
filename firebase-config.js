939f1e17da7c42d62d699e079f83bd797b4266e8
WhtTheHel <karmanuserbot@gmail.com>
2026-04-26 05:13:52 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDlM0PNLlrUejkgW1774fjZDBaDJZT4lDM", 
+  apiKey: "AIzaSyDlM0PNLlrUejkgW1774fjZDBaDJZT4lDM",
   authDomain: "qr-scan-f3702.firebaseapp.com",
   projectId: "qr-scan-f3702",
   storageBucket: "qr-scan-f3702.firebasestorage.app",
   messagingSenderId: "149512346733",
-  appId: "1:149512346733:web:150bf0231cfb79519f3364"
+  appId: "1:149512346733:web:fb24ff0d369e81ec9f3364"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
