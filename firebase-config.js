d7a411221b8ec28e03f6c3c480fbbddefa5080d2
WhtTheHel <karmanuserbot@gmail.com>
2026-04-26 03:46:25 +0700

    Update Firebase configuration settings

--- firebase-config.js
+++ firebase-config.js
@@ -3,12 +3,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSy...", 
-  authDomain: "proyek-anda.firebaseapp.com",
-  projectId: "proyek-anda",
-  storageBucket: "proyek-anda.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyDlM0PNLlrUejkgW1774fjZDBaDJZT4lDM",
+  authDomain: "qr-scan-f3702.firebaseapp.com",
+  projectId: "qr-scan-f3702",
+  storageBucket: "qr-scan-f3702.firebasestorage.app",
+  messagingSenderId: "149512346733",
+  appId: "1:149512346733:web:150bf0231cfb79519f3364"
 };
 
 const app = initializeApp(firebaseConfig);
