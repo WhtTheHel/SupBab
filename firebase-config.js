179d752a8cb775a7efe8d0ea0d6d2812acfe9a42
WhtTheHel <karmanuserbot@gmail.com>
2026-04-26 03:17:37 +0700

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSy...", 
+  authDomain: "proyek-anda.firebaseapp.com",
+  projectId: "proyek-anda",
+  storageBucket: "proyek-anda.appspot.com",
+  messagingSenderId: "123456789",
+  appId: "1:123456789:web:abcdef"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
